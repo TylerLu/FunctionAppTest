954522799599f00d08b24f3d3eeb826ef407e547
TylerLu <admin@tyler.lu>
2016-10-09 10:51:10 +0800

    Refer AutoMapper

---
+++ Src/ClassLibrary1/TestDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class TestDbContext: DbContext
+    {
+        public IMapper Mapper { get; set; }
+    }
+}
