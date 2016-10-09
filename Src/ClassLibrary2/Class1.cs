1276a7121f5f05545eaf4b80652a775c2a6c90d9
TylerLu <admin@tyler.lu>
2016-10-09 11:30:43 +0800

    Add EF

--- Src/ClassLibrary2/Class1.cs
+++ Src/ClassLibrary2/Class1.cs
@@ -1,16 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-//using System.Data.Entity;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ClassLibrary2
 {
-    //public class TestDbContext : DbContext
-    //{
-    //    public DbSet<Class1> Class1 { get; set; }
-    //}
+    public class TestDbContext : DbContext
+    {
+        public DbSet<Class1> Class1 { get; set; }
+    }
 
     public class Class1
     {
