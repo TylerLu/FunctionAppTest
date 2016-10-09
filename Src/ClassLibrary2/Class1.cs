f02af6e5a20373dfec371d92492c56b97040c8c7
TylerLu <admin@tyler.lu>
2016-10-09 11:13:12 +0800

    Refer EF

--- Src/ClassLibrary2/Class1.cs
+++ Src/ClassLibrary2/Class1.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ClassLibrary2
 {
+    public class TestDbContext : DbContext
+    {
+        public DbSet<Class1> Class1 { get; set; }
+    }
+
     public class Class1
     {
+        public int Id { get; set; }
     }
 }
