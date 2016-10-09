a2e315d89be2e809af3378843b8f6124cfa54e36
TylerLu <admin@tyler.lu>
2016-10-09 11:01:10 +0800

    Refer EF

--- Src/ClassLibrary1/TestDbContext.cs
+++ Src/ClassLibrary1/TestDbContext.cs
@@ -1,10 +1,18 @@
 ﻿//using AutoMapper;
-//using System.Data.Entity;
+using System.Data.Entity;
 
-//namespace ClassLibrary1
-//{
-//    public class TestDbContext: DbContext
-//    {
-//        public IMapper Mapper { get; set; }
-//    }
-//}
+namespace ClassLibrary1
+{
+    public class TestDbContext: DbContext
+    {
+        //public IMapper Mapper { get; set; }
+
+        public DbSet<MyClass> MyClass { get; set; }
+    }
+
+    public class MyClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
